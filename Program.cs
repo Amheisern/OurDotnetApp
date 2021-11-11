8938d169b0bd8795cd8bb0fbc9473bbf3ae48561
lodashreload <icarusjapan@gmail.com>
2021-11-10 21:07:29 -0500

    First go at c#

--- Program.cs
+++ Program.cs
@@ -2,11 +2,46 @@
 
 namespace OurDotnetApp
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Program
     {
-      Console.WriteLine("Welcome to C#");
+        static void Main(string[] args)
+        {
+            //implicit 
+            var personname = "Samantha";
+            var score = 95;
+            var otherscore = 100;
+
+            //Explicit
+            string othername = "Tom";
+            int price = 42;
+            var total = score + price;
+
+            bool isexpensive = true;
+            bool isWednesdays = true;
+
+            var sentence = "of all the things i have lost i miss my mind the most";
+
+            var howlong = sentence.Length;
+
+            var rightnow = DateTime.Now;
+
+            Console.Write("What is your name?");
+            var name = Console.ReadLine();
+            Console.Write("It is a pleasure to meet you, ");
+            Console.WriteLine(name);
+
+            var greeting = "It is a pleasure to meet you, " + name + "!";
+            Console.WriteLine(greeting);
+
+            string v = $"It is a pleasure to meet you, {name}";
+
+
+
+
+
+
+
+
+        }
     }
-  }
 }
